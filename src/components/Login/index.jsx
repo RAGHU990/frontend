6c09efc29a1b0765516a144985cb5d9ee09141bc
Raghuram <raghu17122001@gmail.com>
2023-03-16 14:59:25 +0530

    fix register bugs

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,30 +1,25 @@
 import { useState } from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
-const Signup = () => {
-	const [data, setData] = useState({
-		firstName: "",
-		lastName: "",
-		email: "",
-		password: "",
-	});
+const Login = () => {
+	const [data, setData] = useState({ email: "", password: "" });
 	const [error, setError] = useState("");
-	const navigate = useNavigate();
 
 	const handleChange = ({ currentTarget: input }) => {
 		setData({ ...data, [input.name]: input.value });
 	};
 
+
+	
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:8080/register";
+			const url = "http://localhost:8080/api/auth";
 			const { data: res } = await axios.post(url, data);
-			console.log("datafrom  backend",data)
-			navigate("/login");
-			console.log(res.message);
+			localStorage.setItem("token", res.data);
+			window.location = "/";
 		} catch (error) {
 			if (
 				error.response &&
@@ -37,37 +32,11 @@ const Signup = () => {
 	};
 
 	return (
-		<div className={styles.signup_container}>
-			<div className={styles.signup_form_container}>
+		<div className={styles.login_container}>
+			<div className={styles.login_form_container}>
 				<div className={styles.left}>
-					<h1>Welcome Back</h1>
-					<Link to="/login">
-						<button type="button" className={styles.white_btn}>
-							Sign in
-						</button>
-					</Link>
-				</div>
-				<div className={styles.right}>
 					<form className={styles.form_container} onSubmit={handleSubmit}>
-						<h1>Create Account</h1>
-						<input
-							type="text"
-							placeholder="First Name"
-							name="firstName"
-							onChange={handleChange}
-							value={data.firstName}
-							required
-							className={styles.input}
-						/>
-						<input
-							type="text"
-							placeholder="Last Name"
-							name="lastName"
-							onChange={handleChange}
-							value={data.lastName}
-							required
-							className={styles.input}
-						/>
+						<h1>Login to Your Account</h1>
 						<input
 							type="email"
 							placeholder="Email"
@@ -88,13 +57,21 @@ const Signup = () => {
 						/>
 						{error && <div className={styles.error_msg}>{error}</div>}
 						<button type="submit" className={styles.green_btn}>
-							Sign Up
+							Sign In
 						</button>
 					</form>
+				</div>
+				<div className={styles.right}>
+					<h1>New Here ?</h1>
+					<Link to="/signup">
+						<button type="button" className={styles.white_btn}>
+							Sign Up
+						</button>
+					</Link>
 				</div>
 			</div>
 		</div>
 	);
 };
 
-export default Signup;
+export default Login;
